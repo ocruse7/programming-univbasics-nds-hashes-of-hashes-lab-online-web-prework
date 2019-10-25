7bee4046b8b47292c5d2f1893d3d3246ee9b187e
Owen Cruse <ocruse7@gmail.com>
2019-10-25 17:08:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -20,15 +20,15 @@
 # 
 # PORTION_3 = {
 #   label: "Family",
-   sub_category: {
-     label: "Genus",
-   }
- }
- 
- PORTION_4 = {
-   label: "Species",
-   sub_category: nil
- }
+#   sub_category: {
+#     label: "Genus",
+#   }
+# }
+# 
+# PORTION_4 = {
+#   label: "Species",
+#   sub_category: nil
+# }
 
 def naming_system
   # Remember:
@@ -44,16 +44,22 @@ def naming_system
   multi-hash = { 
     label: "Kingdom",
    sub_category: {
+     
      label: "Phylum",
      sub_category: {
-       label: "Class"
+       
+       label: "Class",
        sub_category: {
-         label: "Order"
+         
+         label: "Order",
          sub_category: {
+           
            label: "Family",
             sub_category: {
+              
             label: "Genus",
             sub_category: {
+              
               label: "Species",
               sub_category: nil
             }
