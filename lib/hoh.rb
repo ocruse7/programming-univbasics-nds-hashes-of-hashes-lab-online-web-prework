8eedce21ab9d5a975124c7d887a35f996b8c1ebf
Owen Cruse <ocruse7@gmail.com>
2019-10-25 17:06:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,22 +4,22 @@
 #
 # The tests will guide your construction.
 #
-PORTION_1 = {
-   label: "Kingdom",
-   sub_category: {
-     label: "Phylum",
-     sub_category: {
-       label: "Class"
-     }
-   }
- }
- 
- PORTION_2 = {
-   label: "Order"
- }
- 
- PORTION_3 = {
-   label: "Family",
+#PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#    label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
+#   }
+# }
+# 
+# PORTION_2 = {
+#   label: "Order"
+# }
+# 
+# PORTION_3 = {
+#   label: "Family",
    sub_category: {
      label: "Genus",
    }
