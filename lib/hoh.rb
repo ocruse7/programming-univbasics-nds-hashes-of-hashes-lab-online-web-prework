1910a845e4b6a44528483837eda220a089460e02
Owen Cruse <ocruse7@gmail.com>
2019-10-25 16:59:11 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -42,7 +42,7 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
   multi-hash = { PORTION_1 }
-  multi-hash[:sub_category][:sub_category][:sub_category] = {PORTION_2}
+  #multi-hash[:sub_category][:sub_category][:sub_category] = {PORTION_2}
   #multi-hash[:sub_category][:sub_category][:sub_category][:sub_category] = {PORTION_3}
   #multi-hash[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = {PORTION_4}
   multi-hash
